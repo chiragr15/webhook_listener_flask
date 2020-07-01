4736c8adbeb46c698e562dbc7f681e26efa64c56
Chirag <chiragrudresh15@gmail.com>
2020-07-01 17:55:43 +0530

    webhook_listener

--- jreq.py
+++ jreq.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, redirect, url_for, render_template
+import json
 
 app = Flask(__name__, template_folder='template')
 
@@ -6,17 +7,30 @@ app = Flask(__name__, template_folder='template')
 def alerts():
 	if request.method == 'POST':
 		req_data = request.get_json()
-		alert = req_data['alerts']
-		with open('template/notif.html','w') as f:
-			write_content = ''
-			for keys in alert:
-				write_content += '<p>'
-				for vals in keys:
-					write_content += '<h5>'+ keys[vals] + '</h5>'
-				write_content+= '</p>'
-			f.write(write_content)
-		
-		return ''
+		#jdict = json.loads(req_data)
+		metric_alert = 'alerts' in req_data
+		lods_alert = 'msg' in req_data
+		if metric_alert:
+			metrics_alert = req_data['alerts']
+			with open('template/notif.html','w') as m:
+				write_content = ''
+				for keys in metrics_alert:
+					write_content += '<p>'
+					for vals in keys:
+						write_content += '<h5>'+ keys[vals] + '</h5>'
+					write_content+= '</p>'
+				m.write(write_content)
+			
+			return ''
+		elif lods_alert:
+			with open('template/notif.html','w') as l:
+				content = ''
+				for logvals in req_data:
+					content += '<p>'
+					content += '<h5>' + str(req_data[logvals]) + '</h5>'
+					content += '</p>'
+				l.write(content)
+			return ''	
 
 	return render_template("notif.html")
 
